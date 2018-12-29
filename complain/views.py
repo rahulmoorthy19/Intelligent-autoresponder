3bb74d387e82f4cdbb8f66d4293da3d63634cf63
rahul <rahulmoorthy9.6@gmail.com>
2018-12-29 11:48:09 +0530

    Spam Filterer Updated

--- complain/views.py
+++ complain/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Username,query,team
 import import_ipynb
 from .predict import predict3
+from .spam_filterer import spam_predict
 import mtranslate
 from langdetect import detect
 from django.conf import settings
@@ -17,7 +18,7 @@ complain_path="/home/sirzechlucifer/machine learning/tcs inframind/IntelligentAu
 service_path="/home/sirzechlucifer/machine learning/tcs inframind/IntelligentAutoresponder/autoresponder/complain/servicereq.txt"
 
 
-def complain_page(request):
+def complain_page(request):					##This View is used to take the Query From the User
 	if request.method == 'POST':
 		complain.name= request.POST.get('name')
 		complain.phone_no= request.POST.get('mobilenumber')
@@ -30,61 +31,38 @@ def complain_page(request):
 		else:
 			question.sys_conf=0
 		question.language=detect(request.POST.get('query_que'))
-		question.query_type=predict(request.POST.get('query_que'),complain_path,service_path)
-		question.save()
-		subject=request.POST.get('query_type')+" Problem"
-		query_question=request.POST.get('query_que')
-		email(request,subject,query_question)
-		return HttpResponseRedirect('thankyou_page/')  
+		question.query_type=predict(request.POST.get('query_que'),complain_path,service_path) #detecting complain or service
+		spam_check=spam_predict(question.query_ques)	##This function is used to detect is the query is spam or not
+		if spam_check==0:
+			question.save()
+			subject=request.POST.get('query_type')+" Problem"
+			query_question=request.POST.get('query_que')
+			email(request,subject,query_question)
+			return HttpResponseRedirect('thankyou_page/')
+		else:
+			return render(request, 'complainportal/complain_page.html') 
 	else:
 		return render(request, 'complainportal/complain_page.html')
 
 
-def thankyou_page(request):
+def thankyou_page(request):#This view is used for sending the reply in desired language depending upon question present in KB
 	similarity={}
-	team_name={}
-	team_list={}
 	queries=query.objects.filter(answered_flag=0,sys_conf=question.sys_conf,query_type=question.query_type)
 	for x in queries:
 		similarity[x.query_id]=predict1(question.query_ques,x.query_ques)
-	id_of_answer=max(similarity.items(), key=operator.itemgetter(1))[0]
-	similarity_of_answer=similarity[id_of_answer]
-	if similarity_of_answer>80:
-		answer_queries=query.objects.filter(answered_flag=1,sys_conf=question.sys_conf,query_type=question.query_type)
-		ref_query=query.objects.get(query_id=id_of_answer)
-		for y in answer_queries:
-			if y.query_ques==question.query_ques:
-				lang=y.language
-				y.answer=ref_query.answer
-				y.answered_flag=0
-				reply="The answer to your query id "+str(y.query_id)+" is "+ref_query.answer
-				subject="Solution of query id "+str(y.query_id)
-				revert(request,mtranslate.translate(subject,lang,"auto"),mtranslate.translate(reply,lang,"auto"))
-				y.save()
+	if bool(similarity):
+		id_of_answer=max(similarity.items(), key=operator.itemgetter(1))[0]
+		similarity_of_answer=similarity[id_of_answer]
 	else:
-		answer_queries=query.objects.filter(answered_flag=1,sys_conf=question.sys_conf,query_type=question.query_type)
-		for z in answer_queries:
-			if z.query_ques==question.query_ques:
-				lang=z.language
-				team_assign=team.objects.filter(team_type=question.query_type)
-				for w in team_assign:
-					team_name[w.team_id]=w.workload	
-					team_list[w.team_id]=w.team_name
-				z.team_assigned_id_id=min(team_name.items(), key=operator.itemgetter(1))[0]
-				id_of_ques=z.query_id
-				z.save()
-				team_assign=team.objects.filter(team_id=z.team_assigned_id_id)
-				for m in team_assign:
-					no_days=predict3(list([str(question.query_ques),int(m.team_type),int(m.workload)]))
-					reply="Your query id "+str(id_of_ques)+" has been assigned to team "+m.team_name+" and will be solved in"+str(no_days)
-					subject="Query Solving process undergoing for query id "+str(id_of_ques)
-					revert(request,mtranslate.translate(subject,lang,"auto"),mtranslate.translate(reply,lang,"auto"))
-					m.workload=m.workload+1
-					m.save()
+		similarity_of_answer=0
+	if similarity_of_answer>80:
+		similarity_present(id_of_answer,request)
+	else:
+		similarity_notpresent(request)
 	return render(request, 'complainportal/thankyou_page.html')
 
 						
-def email(request,subject,query_question):
+def email(request,subject,query_question): #Used for sending email from Customer to Service Desk
 
 	email_from = settings.EMAIL_HOST_USER
 	recipient_list = ['rahulmoorthy9.6@gmail.com']
@@ -92,7 +70,7 @@ def email(request,subject,query_question):
 	return render(request,'complainportal/complain_page.html')
 
 
-def revert(request,subject,reply):
+def revert(request,subject,reply): #Used for Replying back from Service desk to Customer 
 	
 	email_host = 'smtp.gmail.com'
 	host_port = 587
@@ -108,3 +86,42 @@ def revert(request,subject,reply):
 	connection.close()
 	return render(request,'complainportal/complain_page.html')
 
+
+def similarity_present(id_of_answer,request):# Used when question present in KB
+	answer_queries=query.objects.filter(answered_flag=1,sys_conf=question.sys_conf,query_type=question.query_type)
+	ref_query=query.objects.get(query_id=id_of_answer)
+	for y in answer_queries:
+		if y.query_ques==question.query_ques:
+			lang=y.language
+			y.answer=ref_query.answer
+			y.answered_flag=0
+			reply="The answer to your query id "+str(y.query_id)+" is "+ref_query.answer
+			subject="Solution of query id "+str(y.query_id)
+			revert(request,mtranslate.translate(subject,lang,"auto"),mtranslate.translate(reply,lang,"auto"))
+			y.save()
+
+
+def similarity_notpresent(request): #Used when question not present in KB
+	team_name={}
+	team_list={}
+	answer_queries=query.objects.filter(answered_flag=1,sys_conf=question.sys_conf,query_type=question.query_type)
+	for z in answer_queries:
+		if z.query_ques==question.query_ques:
+			lang=z.language
+			team_assign=team.objects.filter(team_type=question.query_type)
+			for w in team_assign:
+				team_name[w.team_id]=w.workload	
+				team_list[w.team_id]=w.team_name
+			z.team_assigned_id_id=min(team_name.items(), key=operator.itemgetter(1))[0]
+			id_of_ques=z.query_id
+			z.save()
+			team_assign=team.objects.filter(team_id=z.team_assigned_id_id)
+			for m in team_assign:
+				no_days=predict3(list([str(question.query_ques),int(m.team_type),int(m.workload)]))#no of days
+				reply="Your query id "+str(id_of_ques)+" has been assigned to team "+m.team_name+" and will be solved in "+str(no_days)+" days"
+				subject="Query Solving process undergoing for query id "+str(id_of_ques)
+				revert(request,mtranslate.translate(subject,lang,"auto"),mtranslate.translate(reply,lang,"auto"))
+				m.workload=m.workload+1
+				m.save()
+	
+	
